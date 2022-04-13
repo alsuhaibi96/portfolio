2a1ccf2e89a71c7bc1600bdcff91d04759d35a11
Alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2022-04-13 19:36:40 +0300

    saving files

---
+++ js/dictionary.js
@@ -0,0 +1,106 @@
+var dictionary = {
+    'home': {  
+        'en': 'HOME',
+        'ar': 'الرئيسية',
+    },
+    'about': {  
+        'en': 'ABOUT',
+        'ar': 'عني',
+    },
+    'services': {  
+        'en': 'SERVICES',
+        'ar': 'خدماتنا',
+    },
+    'RESUME': {  
+        'en': 'RESUME',
+        'ar': 'السيرة الذاتية',
+    },
+    'ChangeLanguage': {  
+        'en': 'CHANGE LANGAUGE',
+        'ar': 'تغيير اللغة',
+    },
+   
+    'Hello': {  
+        'en': 'Hello !',
+        'ar': '! مرحباًً',
+    },
+    'introduction': {  
+        'en': '  <span class="myname-span" >I Am Abdulrahman Alsuhaibi</span><br><br> I am a Full-stack Developer.<br> I build web and mobile apps using modern FrameWorks and they are : "Laravel PHP FrameWork" for Web Development and "Flutter" for Mobile development.',
+        'ar': '  <span class="myname-span " style="">انا عبدالرحمن الصهيبي</span><br><br> .مطور مواقع إلكترونية وتطبيقات الهاتف  <br> اقوم ببناء تطبيقات الويب والهاتف بإستخدام احدث اطارات التقنيات البرمجية  مثل : " لارافل " لبناء تطبيقات الويب و " فلاتر " لبناء تطبيقات الهاتف' ,
+    },
+    
+    'aboutMeLabel': {  
+        'en': 'About me',
+        'ar': 'عني',
+    },
+    'aboutMeContent': {  
+        'en': 'Full Stack Developer with experience in Web and Mobile development, developing, and   implementing applications and solutions using a range of modern technologies and programming languages.     Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Full-stack Developer.',
+        'ar': 'لدي خبرة جيدة في تطوير تطبيقات الهاتف والمواقع الإلكترونية حيث استطيع تطوير وبناء التطبيقات بإستخدام احدث التقنيات واللغات البرمجية . اطمح للحصول على خبرة كبيرة في التطوير  والتنافس كمطور للمواقع وتطبيقات الهاتف.',
+    },
+    'name': {  
+        'en': 'Name :',
+        'ar': 'الإسم :',
+    },
+    'nameContent': {  
+        'en': ' Abdulrahman Alsuhaibi',
+        'ar': 'عبدالرحمن الصهيبي ',
+    },
+    'address': {  
+        'en': 'Address :',
+        'ar': 'العنوان :',
+    },
+    'addressContent': {  
+        'en': ' Yemen - Sana\'a',
+        'ar': 'اليمن - صنعاء',
+    },
+    'phoneNumber': {  
+        'en': 'Phone Number :',
+        'ar': 'رقم الهاتف :',
+    },
+    'phoneNumberContent': {  
+        'en': '+967775474720',
+        'ar': '967775474720+',
+    },
+    'email': {  
+        'en': 'Email :',
+        'ar': 'الايميل :',
+    },
+    'emailContent': {  
+        'en': 'alsuhaibiabdulrahman@gmail.com',
+        'ar': 'alsuhaibiabdulrahman@gmail.com',
+    },
+    'ContentLanguage':{
+      'en':'Language Content',
+      'ar':'محتوى اللغة',
+    },
+    
+    'DownloadCv': {  
+        'en': 'Download CV',
+        'ar': 'تحميل السيرة الذاتية',
+    },
+  
+    'services': {  
+        'en': 'SERVICES',
+        'ar': 'خدماتنا',
+    },
+    'ProvidingServices': {  
+        'en': 'We provide services in the following',
+        'ar': ': نوفر خدمات في ',
+    },
+    'webDevelopment': {  
+        'en': 'Web Development',
+        'ar': 'تطوير مواقع الويب',
+    },
+    'mobileDevelopment': {  
+        'en': 'Mobile Development',
+        'ar': ' تطوير تطبيقات الهاتف',
+    },
+   
+    'UI-UX': {  
+        'en': 'UI/UX Design',
+        'ar': 'UI , UX تصاميم ',
+    },
+   
+   
+
+};
---
+++ js/main_js.js
@@ -0,0 +1,132 @@
+function animate(){
+    var element001 =document.getElementById("CV");
+    element001.setAttribute('style','color: red');
+  
+  
+  }
+    function changeTheme() {
+  
+    
+       var element = document.body;
+       element.classList.toggle("theme-mode");
+       var element2=document.getElementById("nav-link-id");
+       var element3=document.getElementById("nav-link-about-id");
+       var element4=document.getElementById("nav-link-services-id");
+       var element5=document.getElementById("nav-link-resume-id");
+       var element6=document.getElementById("changeLanguageBtn") ;
+    
+      //  var element6=document.getElementsByTagName("i");
+  
+       var x = document.getElementById("iconsDivId");
+  var y = x.getElementsByTagName("i");
+  var i;
+  
+  
+       if (document.getElementById('switch-btn').checked) {
+      
+        for (i = 0; i < y.length; i++) {
+    y[i].style.color = "white";
+  } 
+        element2.setAttribute('style','color: white');
+        element3.setAttribute('style','color: white');
+        element4.setAttribute('style','color: white');
+        element5.setAttribute('style','color: white');
+        element6.setAttribute('style','color: white');
+  
+          } else {
+            for (i = 0; i < y.length; i++) {
+    y[i].style.color = "black";
+  } 
+            element2.setAttribute('style','color: black');
+            element3.setAttribute('style','color: black');
+            element4.setAttribute('style','color: black');
+        element5.setAttribute('style','color: black');
+        element6.setAttribute('style','color: black');
+          }
+  
+   
+    }
+   function load(){
+  
+    document.getElementById("switch-btn").checked=true;
+   }
+  
+  
+  
+  
+  var langs = ['en', 'ar'];
+  var current_lang_index = 0;
+  var current_lang = langs[current_lang_index];
+  
+  window.change_lang = function() {
+  
+    current_lang_index = ++current_lang_index % 2;
+      current_lang = langs[current_lang_index];
+     
+     
+   if(current_lang_index==1){
+     
+  var middleContnet=document.getElementById("about-me");
+  
+  middleContnet.setAttribute('style','margin-right: 5%;');
+  
+  // document.getElementById("home").style.textAlign="right";
+  // document.body.style.direction = "rtl";
+  document.getElementById("nav-id").style.direction="rtl";
+  document.getElementById("myInfoDiv").style.textAlign = "right";
+    document.getElementById("iconsDivId").style.textAlign="right"
+    document.getElementById("modal-header-id").style.direction="rtl";
+    document.getElementById("iconsDivId").style.direction="rtl";
+  document.getElementById("about-me").style.direction="rtl";
+  document.getElementById("about-me").style.textAlign = "right";
+  
+  }
+  if(current_lang_index==0){
+    document.getElementById("nav-id").style.direction="ltr";
+    document.getElementById("myInfoDiv").style.textAlign = "left";
+    document.getElementById("iconsDivId").style.textAlign="right"
+    document.getElementById("iconsDivId").style.direction="ltr";
+
+    document.getElementById("about-me").style.direction="ltr";
+  document.getElementById("about-me").style.textAlign = "left";
+  document.getElementById("modal-header-id").style.direction="ltr";
+  
+   }
+   translate();
+  
+    
+  }
+  
+  function translate() {
+   
+      $("[data-translate]").each(function(){
+          var key = $(this).data('translate');
+          $(this).html(dictionary[key][current_lang] || "N/A");
+      });
+  }
+  
+  translate();
+  
+  
+  $(window).on('scroll load',function () {
+  
+      $('.Profilephoto-animation ,.introduction-animation,.about-content-animation')
+      .each(function () {
+          var imagePos = $(this).offset().top;
+          var imageHeight = $(this).height();
+          var topOfWindow = $(window).scrollTop();
+  
+          if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+              $(this).addClass("Profile-animation intro-animation about-content-animate");
+          } else {
+              $(this).removeClass("Profile-animation intro-animation about-content-animate");
+          }
+      })
+      
+      ;
+  });
+  
+  
+
+  
+  
